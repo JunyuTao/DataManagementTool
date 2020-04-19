a78b418009ed499a8040ed7cad6cbf630ebfd5d4
JunyuTao <47371407+JunyuTao@users.noreply.github.com>
2020-04-19 15:11:49 -0500

    changes

--- scripts/auth.js
+++ scripts/auth.js
@@ -8,9 +8,10 @@ adminForm.addEventListener('submit', (e) => {
   const addAdminRole = functions.httpsCallable('addAdminRole');
   addAdminRole({ email: adminEmail }).then(result => {
     console.log(result);
+    window.alert("Success!");
   });
 });
-
+console.log(auth.currentUser)
 // listen for auth status changes
 auth.onAuthStateChanged(user => {
   if (user) {
@@ -19,84 +20,119 @@ auth.onAuthStateChanged(user => {
       setupUI(user);
     });
     db.collection('guides').onSnapshot(snapshot => {
-      setupGuides(snapshot.docs);
+      setupGuides(user, snapshot.docs);
+    }, err => console.log(err.message));
+
+    db.collection('maintenance').onSnapshot(snapshot => {
+      setupMaintenance(user, snapshot.docs);
     }, err => console.log(err.message));
   } else {
     setupUI();
-    setupGuides([]);
+    setupGuides(false, []);
+    setupMaintenance(false, []);
   }
 });
 
-  // create new data
+// create new data
 const createForm = document.querySelector('#create-form');
 createForm.addEventListener('submit', (e) => {
   e.preventDefault();
-  db.collection('guides').add({
-    name: createForm.name.value,
-    software: createForm.software.value,
-    tag_num: createForm.tag_num.value,
-    license_key: createForm.license_key.value,
-    exp_date: createForm.exp_date.date,
+  console.log(createForm)
+
+  if (createForm.tag_num) {
+    let formData = {
+      tag_num: createForm.tag_num.value,
+      owner: createForm.owner.value,
+      laptop_v: createForm.laptop_v.value,
+      repairman : createForm.repairman.value,
+      problem: createForm.problem.value,
+      date_in: createForm.date_in.value,
+      date_out: createForm.date_out.value,
+    }
+    console.log(formData)
+    db.collection('maintenance').add(formData).then(() => {
+      // close the create modal & reset form
+      const modal = document.querySelector('#modal-create');
+      M.Modal.getInstance(modal).close();
+      createForm.reset();
+    }).catch(err => {
+      console.log(err.message);
+    });
+  } else {
+    let formData = {
+      name: createForm.name.value,
+      software: createForm.software.value,
+      tag_num: createForm.tag_num.value,
+      license_key: createForm.license_key.value,
+      exp_date: createForm.exp_date.value,
+    }
+    db.collection('guides').add(formData).then(() => {
+      // close the create modal & reset form
+      const modal = document.querySelector('#modal-create');
+      M.Modal.getInstance(modal).close();
+      createForm.reset();
+    }).catch(err => {
+      console.log(err.message);
+    });
+  }
+});
+
+// signup
+const signupForm = document.querySelector('#signup-form');
+signupForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // get user info
+  const email = signupForm['signup-email'].value;
+  const password = signupForm['signup-password'].value;
+
+  // sign up the user & add firestore data
+  auth.createUserWithEmailAndPassword(email, password).then(cred => {
+    return db.collection('users').doc(cred.user.uid).set({
+      bio: signupForm['signup-bio'].value
+    });
   }).then(() => {
-    // close the create modal & reset form
-    const modal = document.querySelector('#modal-create');
+    // close the signup modal & reset form
+    const modal = document.querySelector('#modal-signup');
     M.Modal.getInstance(modal).close();
-    createForm.reset();
+    signupForm.reset();
+    signupForm.querySelector('.error').innerHTML = '';
   }).catch(err => {
-    console.log(err.message);
+    signupForm.querySelector('.error').innerHTML = err.message;
   });
 });
-  
-  // signup
-  const signupForm = document.querySelector('#signup-form');
-  signupForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    
-    // get user info
-    const email = signupForm['signup-email'].value;
-    const password = signupForm['signup-password'].value;
-  
-    // sign up the user & add firestore data
-    auth.createUserWithEmailAndPassword(email, password).then(cred => {
-      return db.collection('users').doc(cred.user.uid).set({
-        bio: signupForm['signup-bio'].value
-      });
-    }).then(() => {
-      // close the signup modal & reset form
-      const modal = document.querySelector('#modal-signup');
-      M.Modal.getInstance(modal).close();
-      signupForm.reset();
-      signupForm.querySelector('.error').innerHTML = '';
-    }).catch(err => {
-      signupForm.querySelector('.error').innerHTML = err.message;
-    });
-  });
-  
-  // logout
-  const logout = document.querySelector('#logout');
-  logout.addEventListener('click', (e) => {
-    e.preventDefault();
-    auth.signOut();
-  });
-  
-  // login
-  const loginForm = document.querySelector('#login-form');
-  loginForm.addEventListener('submit', (e) => {
-    e.preventDefault();
-    
-    // get user info
-    const email = loginForm['login-email'].value;
-    const password = loginForm['login-password'].value;
-  
-    // log the user in
-    auth.signInWithEmailAndPassword(email, password).then((cred) => {
-      // close the signup modal & reset form
-      const modal = document.querySelector('#modal-login');
-      M.Modal.getInstance(modal).close();
-      loginForm.reset();
-      lohinForm.querySelector('.error').innerHTML = '';
-    }).catch(err => {
-      loginForm.querySelector('.error').innerHTML = err.message;
-    })
-  
+
+// logout
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+  e.preventDefault();
+  auth.signOut();
+});
+// sidenav logout
+const sidenavlogout = document.querySelector('#logoutsidenav');
+sidenavlogout.addEventListener('click', (e) => {
+  e.preventDefault();
+  auth.signOut();
+});
+
+// login
+const loginForm = document.querySelector('#login-form');
+loginForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  // get user info
+  const email = loginForm['login-email'].value;
+  const password = loginForm['login-password'].value;
+
+  // log the user in
+  auth.signInWithEmailAndPassword(email, password).then((cred) => {
+    // close the signup modal & reset form
+    const modal = document.querySelector('#modal-login');
+    M.Modal.getInstance(modal).close();
+    loginForm.reset();
+    lohinForm.querySelector('.error').innerHTML = '';
+  }).catch(err => {
+    loginForm.querySelector('.error').innerHTML = err.message;
   })
+
+})
--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,9 @@
 // DOM elements
 const guideList = document.querySelector('.guides');
+const guideLists = document.querySelector('.guidess');
 const loggedOutLinks = document.querySelectorAll('.logged-out');
 const loggedInLinks = document.querySelectorAll('.logged-in');
-const accountDetails = document.querySelector('.account-details'); 
+const accountDetails = document.querySelector('.account-details');
 const adminItems = document.querySelectorAll('.admin');
 
 
@@ -35,15 +36,17 @@ const setupUI = (user) => {
 };
 
 // setup data
-const setupGuides = (data) => {
-
+const setupGuides = (user, data) => {
+  console.log(data)
   if (data.length) {
     let html = '';
+    var dataArray = [];
     data.forEach(doc => {
-      const guide = doc.data();
+      const guide = { id: doc.id, ...doc.data() };
+      dataArray.push(guide)
       const li = `
         <li>
-          <div class="collapsible-header grey lighten-4"> ${guide.name} </div>  
+          <div class="collapsible-header grey lighten-4"> ${guide.software} </div>  
           <div class="collapsible-body white"> <label for="tag_num" style="font-size:medium; color: rgb(1, 119, 187); font-weight: bold">Tag Number : </label> ${guide.tag_num} </div>
           <div class="collapsible-body white"> <label for="license_key" style="font-size:medium; color: rgb(1, 119, 187); font-weight: bold">License Key : </label> ${guide.license_key} </div>
           <div class="collapsible-body white">  <label for="exp_date" style="font-size:medium; color: rgb(1, 119, 187); font-weight: bold">Expiration Date: </label> ${guide.exp_date} </div>
@@ -51,16 +54,187 @@ const setupGuides = (data) => {
       `;
       html += li;
     });
-    guideList.innerHTML = html
+
+    var groupedGuides = _.groupBy(dataArray, (guide) => {
+      return guide.software;
+    });
+
+    let guidenNames = Object.keys(groupedGuides)
+    let tableHtml = ''
+    guidenNames.forEach(software => {
+      const li = `
+      <li>
+      <div class="collapsible-header grey lighten-4">${software}</div>
+      <div class="collapsible-body white">
+        <table id="${software.replace(/ /g, '')}">
+        <input type="text" id="search${software.replace(/ /g, '')}" onkeyup="search(${software.replace(/ /g, '')})" placeholder="Search" title="Type in a name">
+     
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>License Key</th>
+            <th>Tag Number</th>
+            <th>Expiration Date</th>
+            <th></th>
+          </tr>
+          </thead>
+          <tbody>
+          ${getTable(user.admin, groupedGuides[software])}
+          </tbody>
+        </table>
+      </div>
+    </li>
+      `;
+      tableHtml += li;
+    });
+    guideLists.innerHTML = tableHtml;
+    //guideList.innerHTML = html;
   } else {
-    guideList.innerHTML = '<h3 class="center-align" style="color:white; margin:90px;">Login to view data</h3>';
+    //guideList.innerHTML = ;
+    guideLists.innerHTML = '<h3 class="center-align" style="color:white; margin:90px;">Login to view data</h3>';
   }
-  
+
 
 };
 
+
+const setupMaintenance = (user, data) => {
+  console.log(user)
+  console.log(data)
+  if (data.length) {
+    let html = '';
+    var dataArray = [];
+    data.forEach(doc => {
+      const maintenance = { id: doc.id, ...doc.data() };
+      dataArray.push(maintenance)
+      const li = `
+        <li>
+        <div class="collapsible-header grey lighten-4">${maintenance.tag_num}</div>
+        <div class="collapsible-body white">
+          <table>
+         
+          <thead>
+            <tr>
+              <th>Owner</th>
+              <th>Asset Number</th>
+              <th>Repairman</th>
+              <th>Laptop Version</th>
+              <th>Problem</th>
+              <th>Date In</th>
+              <th>Date Out</th>
+              <th></th>
+          
+              </tr>
+            </thead>
+            <tbody>
+            <tr>
+           <td>${maintenance.owner}</td>
+           <td>${maintenance.tag_num}</td>
+           <td>${maintenance.repairman}</td>
+           <td>${maintenance.laptop_v}</td>
+           <td>${maintenance.problem}</td>
+           <td>${maintenance.date_in}</td>
+           <td>${maintenance.date_out}</td>
+           ${mhowDelete(maintenance, user.admin)}
+          </tr>
+            </tbody>
+          </table>
+        </div>
+      </li>
+        `;
+      html += li;
+    });
+    guideList.innerHTML = html;
+  } else {
+    //guideList.innerHTML = ;
+    guideLists.innerHTML = '<h3 class="center-align" style="color:white; margin:90px;">Login to view data</h3>';
+  }
+}
+
+
+const getTable = (isAdmin, rows) => {
+  let rowHtml = ''
+  rows.forEach(row => {
+    let rHrml = `<tr>
+           <td>${row.name}</td>
+           <td>${row.license_key}</td>
+           <td>${row.tag_num}</td>
+           <td>${row.exp_date}</td>
+           ${showDelete(row, isAdmin)}
+          </tr>`
+
+    rowHtml += rHrml
+  })
+  return rowHtml
+
+}
+
+const showDelete = (row, isAdmin) => {
+  if (isAdmin) {
+    return '<td> <button onclick=deleteRow(\'' + row.id + '\')>Delete</button></td>';
+  } else {
+    return ``
+  }
+}
+
+const mhowDelete = (row, isAdmin) => {
+  if (isAdmin) {
+    return '<td> <button onclick=mdeleteRow(\'' + row.id + '\')>Delete</button></td>';
+  } else {
+    return ``
+  }
+}
+const mdeleteRow = (rowId) => {
+  db.collection('maintenance').doc(rowId).delete();
+}
+
+const deleteRow = (rowId) => {
+  db.collection('guides').doc(rowId).delete();
+}
+
+const search = (table) => {
+  console.log(table)
+  let input, filter, tr, td, i, txtValue;
+  input = document.getElementById("search" + table.id);
+  filter = input.value.toUpperCase();
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      console.log(td)
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+const msearch = () => {
+  var input, filter, table, li, td, i, txtValue;
+  input = document.getElementById("mSearch");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("mtable");
+  li = table.getElementsByTagName("li");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < li.length; i++) {
+    td = li[i].getElementsByTagName("div")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+      } else {
+        li[i].style.display = "none";
+      }
+    }
+  }
+}
+
 // setup materialize components
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
 
   var modals = document.querySelectorAll('.modal');
   M.Modal.init(modals);
